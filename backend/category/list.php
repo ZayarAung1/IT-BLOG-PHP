fd88d7fc91854905f77b2c6315642d63b796c533
Zayar Aung <shwethwey001@gmail.com>
2025-06-04 17:28:06 +0630

    cate

--- backend/category/list.php
+++ backend/category/list.php
@@ -13,13 +13,13 @@
     <title>SB Admin 2 - Blank</title>
 
     <!-- Custom fonts for this template-->
-    <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link href="../vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
     <link
         href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
         rel="stylesheet">
 
     <!-- Custom styles for this template-->
-    <link href="css/sb-admin-2.min.css" rel="stylesheet">
+    <link href="../css/sb-admin-2.min.css" rel="stylesheet">
 
 </head>
 
@@ -29,7 +29,7 @@
     <div id="wrapper">
 
         <!-- Sidebar -->
-        <?php include('sidebar.php'); ?>
+        <?php include('../sidebar.php'); ?>
         <!-- End of Sidebar -->
 
         <!-- Content Wrapper -->
@@ -61,7 +61,7 @@
                     </form>
 
                     <!-- Topbar Navbar -->
-                    <?php include('navbar.php'); ?>
+                    <?php include('../navbar.php'); ?>
                     <!-- End of Topbar Navbar -->   
                 
 
@@ -69,7 +69,7 @@
                 <div class="container-fluid">
 
                     <!-- Page Heading -->
-                    <h1 class="h3 mb-4 text-gray-800">Dashboard</h1>
+                    <h1 class="h3 mb-4 text-gray-800">Category list page</h1>
 
                 </div>
                 <!-- /.container-fluid -->
@@ -78,7 +78,7 @@
             <!-- End of Main Content -->
 
             <!-- Footer -->
-            <?php include('footer.php'); ?>
+            <?php include('../footer.php'); ?>
             <!-- End of Footer -->
 
         </div>
@@ -113,14 +113,14 @@
     </div>
 
     <!-- Bootstrap core JavaScript-->
-    <script src="vendor/jquery/jquery.min.js"></script>
-    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="../vendor/jquery/jquery.min.js"></script>
+    <script src="../vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 
     <!-- Core plugin JavaScript-->
-    <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+    <script src="../vendor/jquery-easing/jquery.easing.min.js"></script>
 
     <!-- Custom scripts for all pages-->
-    <script src="js/sb-admin-2.min.js"></script>
+    <script src="../js/sb-admin-2.min.js"></script>
 
 </body>
 
